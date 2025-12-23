932507cbd452d6ed1777e4633adcb52b50cd2cf5
Mslliksrjun <mallikarjunckm@example.com>
2025-12-23 22:10:51 +0530

    removed the files

---
+++ tests/test_app.py
@@ -0,0 +1,13 @@
+import app.app as calc
+
+def test_addition():
+    assert 2 + 3 == 5
+
+def test_subtraction():
+    assert 5 - 3 == 2
+
+def test_multiplication():
+    assert 2 * 4 == 8
+
+def test_division():
+    assert 10 / 2 == 5
